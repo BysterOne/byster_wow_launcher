b4a3ecfd193c2a083ea68052d90bd59541a3bf73
Alexus16 <zagulin2012@gmail.com>
2022-01-10 03:49:14 +0300

    Дефолт тип загрузки -> 3

--- Byster/Byster/Models/Services/UserInfoService.cs
+++ Byster/Byster/Models/Services/UserInfoService.cs
@@ -160,7 +160,7 @@ namespace Byster.Models.Services
             Branch = Registry.GetValue("HKEY_CURRENT_USER\\Software\\Byster", "Branch", "undefined") as string;
             if(Branch == "undefined") Registry.SetValue("HKEY_CURRENT_USER\\Software\\Byster", "Branch", (Branch = "master"));
             LoadType = Convert.ToInt32(Registry.GetValue("HKEY_CURRENT_USER\\Software\\Byster", "LoadType", -1));
-            if (LoadType == -1) Registry.SetValue("HKEY_CURRENT_USER\\Software\\Byster", "Console", (LoadType = 1));
+            if (LoadType == -1) Registry.SetValue("HKEY_CURRENT_USER\\Software\\Byster", "Console", (LoadType = 3));
             Password = Registry.GetValue("HKEY_CURRENT_USER\\Software\\Byster", "Password", "undefined") as string;
             if (UserType == BranchType.TEST)
             {
